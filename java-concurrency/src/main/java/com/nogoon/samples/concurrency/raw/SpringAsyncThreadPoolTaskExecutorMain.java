ddc7884ea99b01170dbc45e5edbc6d482cc6d457
kyungtaak <kyungtaak@gmail.com>
2012-12-26 16:50:43 +0900

    remove comment

--- java-concurrency/src/main/java/com/nogoon/samples/concurrency/raw/SpringAsyncThreadPoolTaskExecutorMain.java
+++ java-concurrency/src/main/java/com/nogoon/samples/concurrency/raw/SpringAsyncThreadPoolTaskExecutorMain.java
@@ -24,7 +24,6 @@ public class SpringAsyncThreadPoolTaskExecutorMain {
 		try {
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
